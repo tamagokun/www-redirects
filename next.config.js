cf8d91bc7923e42f059fc42d73809d0fdb49ae20
Mike Kruk <mike@ripeworks.com>
2022-09-23 11:23:57 -0400

    set up redirect

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "*",
+        destination: process.env.REDIRECT_URL,
+        permanent: true,
+      },
+    ];
+  },
+};
