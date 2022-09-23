e0dd9c4cc4c09329a2a358e3b8021a53dd205bf2
Mike Kruk <mike@ripeworks.com>
2022-09-23 11:28:32 -0400

    fix source path again

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/*",
+        source: "/:path*",
         destination: process.env.REDIRECT_URL,
         permanent: true,
       },
